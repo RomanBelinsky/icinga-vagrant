b1bcfb487e4b1cd61796e82b673c757545bb882a
rbelinsky <rbelinsky@dalet.com>
2015-05-06 11:40:30 +0300

    install nrpe_check

--- icinga2x/manifests/finalize.sh
+++ icinga2x/manifests/finalize.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+sudo su
+yum install openssl-devel -y
+yum install nano -y
+chmod -R 0777 /etc/icinga2/conf.d/
+echo Installing NRPE
+wget http://liquidtelecom.dl.sourceforge.net/project/nagios/nrpe-2.x/nrpe-2.15/nrpe-2.15.tar.gz
+tar -xvf nrpe-2.15.tar.gz
+cd nrpe-2.15/
+/home/vagrant/nrpe-2.15/configure
+make -C /home/vagrant/nrpe-2.15/
+make -C /home/vagrant/nrpe-2.15/ install	
 echo "The Icinga 2 Vagrant VM has finished installing. See http://localhost:8080 for more details."
